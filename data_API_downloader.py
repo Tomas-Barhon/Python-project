5b2cbb46a7ab2b84e3f6e60679ee36d878631c7a
Tomas-Barhon <tomas.barhon@hotmail.com>
2023-06-21 07:43:52 +0200

    Adding preliminary version of the pipeline class, TODO: fix merge_final_table method

--- data_API_downloader.py
+++ data_API_downloader.py
@@ -3,9 +3,12 @@ import pandas as pd
 import requests
 import numpy as np
 from zipfile import ZipFile
+import geopandas as gp
+from shapely import geometry
+from shapely.geometry import Point
 
 class Downloader:
-    def __init__(self, year, month):
+    def __init__(self, year, month) -> None:
         self.file_name = f"{year}{month}"
 
     def get_request(self):
@@ -39,3 +42,68 @@ class Downloader:
                 if isinstance(unzipped_file,pd.DataFrame):
                     data.append(unzipped_file)
         return pd.concat(data,axis=0,ignore_index=True)
+    
+class Data_pipeline:
+    def __init__(self, crime_data, create_data = True) -> None:
+        self.create_data = create_data
+        self.people_in_polygons = pd.read_excel("počet_obyvatel_ORP.xlsx").dropna()
+        self.people_in_polygons.reset_index(drop=True,inplace=True)
+        self.people_in_polygons.rename(columns = {"Kraje / SO ORP":"ORP_NAZEV","Počet\nobyvatel\ncelkem":"AMMOUNT"},inplace = True)
+        geojson = gp.read_file("ORP_P.shp",encoding = "Windows-1250")
+        self.polygons = geojson.to_crs(epsg=4326)
+        if self.create_data:
+            self.crime_data = crime_data
+        if not self.create_data:
+            self.data_in_polygons = pd.read_csv("data_in_polygons.csv")
+            self.data_in_polygons = self.data_in_polygons.drop(["Unnamed: 0"],axis = 1)
+        
+
+    def match_crime_data_to_polygons(self):
+        if self.create_data:
+            self.crime_data = self.crime_data[(self.crime_data["relevance"] == 3) | (self.crime_data["relevance"] == 4)]
+            self.crime_data = self.crime_data[(self.crime_data["state"] == 1) | (self.crime_data["state"] == 2) | (self.crime_data["state"] == 3) | (self.crime_data["state"] == 4)]
+            self.crime_data= self.crime_data[(self.crime_data["types"] >= 18) & (self.crime_data["types"] <= 62)]
+            self.crime_data["ORP"] = np.nan
+            self.crime_data["points"] = self.crime_data.apply(lambda row: Point(row["x"],row["y"]),axis=1)
+            for indx,point in enumerate(self.crime_data["points"]):
+                for region_name,polygon in zip(self.polygons["NAZEV"],self.polygons["geometry"]):
+                    if point.within(polygon):
+                        self.crime_data.iloc[indx,8] = region_name
+                        break
+            self.data_in_polygons = self.crime_data
+        if not self.create_data:
+            self.data_in_polygons = pd.read_csv("data_in_polygons.csv")
+            self.data_in_polygons = self.data_in_polygons.drop(["Unnamed: 0"],axis = 1)
+        
+    def compute_counts_per_polygon(self):
+        counts = self.data_in_polygons["ORP"].value_counts().to_frame()
+        counts.reset_index(inplace=True)
+        self.counts = counts.rename(columns = {'index':'ORP',"ORP":"counts"})
+        
+    def preprocess_paq_data(self):
+        self.paq_data = pd.read_csv("Data-pro-Python-DataPAQ.csv")
+        self.paq_data.drop(['Propadání (průměr 2015–2021) / Průměr ČR [%]',
+       'Propadání (průměr 2015–2021) / Průměr kraje [%]',
+       'Propadání (průměr 2015–2021) / Průměr okresu [%]',
+       'Propadání (průměr 2015–2021) / Průměr sociálně podobných ORP [%]',
+       'Názvy sociálně podobných ORP', 'Kódy sociálně podobných ORP','Domácnosti čerpající přídavek na živobytí (2020) / Průměr ČR [%]',
+       'Domácnosti čerpající přídavek na živobytí (2020) / Průměr kraje [%]',
+       'Domácnosti čerpající přídavek na živobytí (2020) / Průměr okresu [%]',
+       'Domácnosti čerpající přídavek na živobytí (2020) / Průměr sociálně podobných ORP [%]','Podíl lidí bez středního vzdělání (2021) / Průměr ČR [%]',
+       'Podíl lidí bez středního vzdělání (2021) / Průměr kraje [%]',
+       'Podíl lidí bez středního vzdělání (2021) / Průměr okresu [%]',
+       'Podíl lidí bez středního vzdělání (2021) / Průměr sociálně podobných ORP [%]','Lidé v exekuci (2021) / Průměr ČR [%]',
+       'Lidé v exekuci (2021) / Průměr kraje [%]',
+       'Lidé v exekuci (2021) / Průměr okresu [%]',
+       'Lidé v exekuci (2021) / Průměr sociálně podobných ORP [%]','Kód ORP','Kód okresu', 'Název okresu', 'Kód kraje',
+       'Název kraje'],axis = 1,inplace=True)
+        self.paq_data.replace(to_replace="Praha",value="Hlavní město Praha",inplace=True)
+
+    def merge_final_table(self):
+        self.final_table = self.polygons.merge(self.counts,how="left",left_on=["NAZEV"],right_on=["ORP"])
+        self.final_table = self.final_table.merge(self.paq_data,how="left",left_on=["NAZEV"],right_on=["Název ORP"])
+        self.final_table = self.final_table.merge(self.people_in_polygons,how="left",left_on=["NAZEV"],right_on=["ORP_NAZEV"])
+        self.final_table = self.final_table.fillna(0)
+        self.final_table = self.final_table["counts/per_person"] = self.final_table["counts"]/self.final_table["AMMOUNT"]
+        self.final_table = self.final_table.replace(to_replace=np.inf,value=0)
+        return self.final_table
